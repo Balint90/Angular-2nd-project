08591ddc19ff363f1faa7b3eeedfbdf1aefba495
Bálint Csertán <csertanb@gmail.com>
2025-05-04 23:04:00 +0200

    Add UserComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
 
 //Decorator
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
